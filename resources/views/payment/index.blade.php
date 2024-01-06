3d0c4255216473f90653c51c0de4b06ddbe3f297
Giancarlo Canessa <canessa@hey.com>
2024-01-06 16:32:51 -0600

    fix issue displaying amount of money owed to kid

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -10,15 +10,30 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
-                    <p class="mb-6">
-                        You are owed: &#36;{{ number_format($payments - $payouts, 2) }}
+                    <p class="mb-6 text-center text-2xl">
+                        &#36;{{ number_format(($paymentTotal - $payoutTotal), 2) }}<br>
+                        You are owed
                     </p>
 
-                    {{-- <ul class="list list-outside list-disc ml-6">
-                        @forelse ($payouts as $payout)
+                    <p class="mb-6">
+                        In total, you have earned &#36;{{ number_format($paymentTotal, 2) }} from reading and have been paid, &#36;{{ number_format($payoutTotal, 2) }}.
+                    </p>
+
+                    {{-- <h1>Earnings</h1>
+                    <ul class="list list-outside list-disc ml-6">
+                        @forelse ($payments as $payment)
                             <li class="mb-4">{{ $payment->created_at }} = &#36;{{ number_format($payment->payout, 2) }}</li>
                         @empty
-                            <li>You don't have any payments yet.</li>
+                            <li>You don't have any earnings yet.</li>
+                        @endforelse
+                    </ul>
+
+                    <h1>Payouts</h1>
+                    <ul class="list list-outside list-disc ml-6">
+                        @forelse ($payouts as $payout)
+                            <li class="mb-4">{{ $payout->created_at }} = &#36;{{ number_format($payout->payout, 2) }}</li>
+                        @empty
+                            <li>You don't have any payouts yet.</li>
                         @endforelse
                     </ul> --}}
 
